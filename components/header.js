6e15e2338d6aef718fa9234f349e9cef614ce312
Andrew Wladis <andrewwladis@gmail.com>
2023-11-12 14:29:16 -0500

    Sunday, November 12th, 2023

--- components/header.js
+++ components/header.js
@@ -183,8 +183,6 @@ const Header = ({ data = {}, isTransparent, onSetup = () => {} }) => {
                         useMegaNav
                       />
                     )}
-
-                    <CartToggle />
                   </div>
                 </div>
               </nav>
