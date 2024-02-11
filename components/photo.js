dd02a8ee779ae8cbf30eedea3a7f0f82d5327558
Andrew Wladis <andrewwladis@gmail.com>
2024-02-11 14:53:49 -0500

    Rounded corners of photos

--- components/photo.js
+++ components/photo.js
@@ -77,47 +77,48 @@ const Photo = ({
 
   return (
     <figure className={className ? className : null} style={
-      (photo.alt == "Instructor/Student" && (screenSize <= 700)) ? 
-      {
-        width: '100%',
-        height: '30vh',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        alignSelf: 'center',
-        padding: '0',
-        margin: '0'
-      }
-      :
-      (photo.alt == "Instructor/Student" && (1100 >= screenSize > 700)) ? 
-      {
-        width: '30vw',
-        alignSelf: 'center',
-      }
-      :
-      (photo.alt == "Instructor/Student" && (screenSize > 1100)) ? 
-      {
-        alignSelf: 'center',
-        width: '20vw',
-        maxWidth: 350,
-        maxHeight: 350,
-        margin: 0,
-        padding: 0,
-      }
-      :
-      (photo.alt == "Medium Post") ? 
-      {
-        width: '20vw',
-        maxWidth: 375
-      }
-      :
-      (photo.alt == "Company") ? 
-      {
-        width: '20vw',
-        maxWidth: 350
-      }
-      :
-      {}
+      (photo.alt == "Instructor/Student" && (screenSize <= 700)) ?
+        {
+          width: '100%',
+          height: '30vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          alignSelf: 'center',
+          padding: '0',
+          margin: '0'
+        }
+        :
+        (photo.alt == "Instructor/Student" && (1100 >= screenSize > 700)) ?
+          {
+            width: '30vw',
+            alignSelf: 'center',
+          }
+          :
+          (photo.alt == "Instructor/Student" && (screenSize > 1100)) ?
+            {
+              alignItems: 'center',
+              justifyContent: 'center',
+              width: '20vw',
+              maxWidth: 350,
+              maxHeight: 350,
+              margin: 0,
+              padding: 0,
+            }
+            :
+            (photo.alt == "Medium Post") ?
+              {
+                width: '20vw',
+                maxWidth: 375
+              }
+              :
+              (photo.alt == "Company") ?
+                {
+                  width: '20vw',
+                  maxWidth: 350
+                }
+                :
+                {}
     }>
       <div
         className={cx('ar', {
@@ -125,28 +126,28 @@ const Photo = ({
         })}
         style={{
           ...aspectCustom,
-          ...(photo.alt == "Instructor/Student" && (screenSize <= 700)) ? 
-          {
-            width: '30vw',
-            height: '30vh',
-            
-          }
-          :
-      (photo.alt == "Medium Post") ? 
-      {
-        width: '20vw',
-        maxWidth: 350
-      }
-          :
-          {}
+          ...(photo.alt == "Instructor/Student" && (screenSize <= 700)) ?
+            {
+              width: '30vw',
+              height: '30vh',
+
+            }
+            :
+              (photo.alt == "Medium Post") ?
+                {
+                  width: '20vw',
+                  maxWidth: 350
+                }
+                :
+                {}
         }}
       >
-          <img
-            ref={observe}
-            width={width}
-            height={height}
-            style={
-              (photo.alt == "Instructor/Student" && (screenSize <= 700)) ? 
+        <img
+          ref={observe}
+          width={width}
+          height={height}
+          style={
+            (photo.alt == "Instructor/Student" && (screenSize <= 700)) ?
               {
                 width: '30vw',
                 height: '30vh',
@@ -155,46 +156,47 @@ const Photo = ({
                 maxWidth: '30vw',
                 maxHeight: '30vh',
                 objectFit: 'contain',
-                
+
               }
               :
-              (photo.alt == "Instructor/Student" && (1100 >= screenSize > 700)) ? 
-              {
-                minWidth: '30vw',
-                minHeight: '30vh',
-                objectFit: 'contain'
-              }
-              :
-              (photo.alt == "Instructor/Student" && (screenSize > 1100)) ? 
-              {
-                width: '20vw',
-                maxWidth: 350,
-                maxHeight: 350,
-                margin: 0,
-                padding: 0,
-              }
-              :
-              (photo.alt == "Medium Post") ? 
-              {
-                maxWidth: 350
-              }
-              :
-              (photo.alt == "Company") ? 
-              {
-                minWidth: '20vw',
-                objectFit: 'contain'
-              }
-              :
-              {}
-            }
-            src={forceLoad || inView ? src : null}
-            srcSet={forceLoad || inView ? srcset : null}
-            sizes={sizes}
-            decoding="async"
-            onLoad={handleLoad}
-            alt={photo.alt || photo.asset?.altText}
-            className={cx(getSize(layout), { 'is-loaded': isLoaded })}
-          />
+              (photo.alt == "Instructor/Student" && (1100 >= screenSize > 700)) ?
+                {
+                  minWidth: '30vw',
+                  minHeight: '30vh',
+                  objectFit: 'contain'
+                }
+                :
+                (photo.alt == "Instructor/Student" && (screenSize > 1100)) ?
+                  {
+                    width: '20vw',
+                    maxWidth: 350,
+                    maxHeight: 350,
+                    margin: 0,
+                    padding: 0,
+                    objectFit: 'contain',
+                  }
+                  :
+                  (photo.alt == "Medium Post") ?
+                    {
+                      maxWidth: 350
+                    }
+                    :
+                    (photo.alt == "Company") ?
+                      {
+                        minWidth: '20vw',
+                        objectFit: 'contain'
+                      }
+                      :
+                      {}
+          }
+          src={forceLoad || inView ? src : null}
+          srcSet={forceLoad || inView ? srcset : null}
+          sizes={sizes}
+          decoding="async"
+          onLoad={handleLoad}
+          alt={photo.alt || photo.asset?.altText}
+          className={cx(getSize(layout), { 'is-loaded': isLoaded })}
+        />
 
         {hasPlaceholder && (
           <div className={cx('ar--placeholder', { 'is-loaded': isLoaded })}>
